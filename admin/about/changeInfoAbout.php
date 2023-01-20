94457ed1c537c4343d39e3bfba1c79bf3f366eba
levifeyd <kuleshov-as@mail.ru>
2023-01-20 11:41:46 +0400

    add error empty file

--- admin/about/changeInfoAbout.php
+++ admin/about/changeInfoAbout.php
@@ -7,15 +7,19 @@
             if (preg_match("/$item$/", $_FILES['im']['name'])) exit("Расширение файла не подходит");
         }
         // вычисляем размер tmp_ времменное имя файла с которым он был сохранен на сервере
-        $type = getimagesize($_FILES['im']['tmp_name']);
-        if ($type && ($type['mime'] != 'image/png' || $type['mime'] != 'image/jpg' || $type['mime'] != 'image/jpeg')) {
-            if ($_FILES['im']['name'] < 1024 * 1000) { // проверяем размер загружамемого файла
-                $upload = '../img/'.$_FILES['im']['name']; // файл будет нахоидтся по этому адресу
-                //далее пермещаем файл в папку(upload)
-                if (move_uploaded_file($_FILES['im']['tmp_name'], $upload)) echo "Файл загружен";
-                else echo "Ошибка при загрузке файла";
-            } else exit("Размер файла превышен");
-        } else exit("Тип файла не подходит");
+        if (empty($_FILES['im']['tmp_name'])) {
+            exit ("Пожалуйста загрузите файл.");
+        } else {
+            $type = getimagesize($_FILES['im']['tmp_name']);
+            if ($type && ($type['mime'] != 'image/png' || $type['mime'] != 'image/jpg' || $type['mime'] != 'image/jpeg')) {
+                if ($_FILES['im']['name'] < 1024 * 1000) { // проверяем размер загружамемого файла
+                    $upload = '../img/' . $_FILES['im']['name']; // файл будет нахоидтся по этому адресу
+                    //далее пермещаем файл в папку(upload)
+                    if (move_uploaded_file($_FILES['im']['tmp_name'], $upload)) echo "Файл загружен";
+                    else echo "Ошибка при загрузке файла.";
+                } else exit("Размер файла превышен.");
+            } else exit("Тип файла не подходит.");
+        }
     }
 ?>
 <?php
